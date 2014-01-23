06643d161403f44d36e039413d955e1af5e1ef4a
Aliaksandr_Pleski <fullzulu@gmail.com>
2014-01-23 17:33:58 +0300

    added project structure and first version of app

---
+++ src/main/java/wkl/training/mvn/runner/Main.java
@@ -0,0 +1,19 @@
+package wkl.training.mvn.runner;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	Logger log = LogManager.getLogger(Main.class);
+    	log.info("THIS IS MY LOG MESSAGE");
+        System.out.println( "Hello World!" );
+    }
+}

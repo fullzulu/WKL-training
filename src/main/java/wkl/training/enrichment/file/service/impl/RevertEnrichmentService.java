2202ba7d0530b95e3850f48f8329a63dbd1a1cc1
Aliaksandr_Pleski <fullzulu@gmail.com>
2014-01-25 02:26:43 +0300

    added revert appender functionality

---
+++ src/main/java/wkl/training/enrichment/file/service/impl/RevertEnrichmentService.java
@@ -0,0 +1,26 @@
+package wkl.training.enrichment.file.service.impl;
+
+import java.io.File;
+import java.io.IOException;
+
+import wkl.training.enrichment.file.service.IEnrichmentService;
+import wkl.training.enrichment.util.FileUtils;
+
+/**
+ * 
+ * @author Aliaksandr_Pleski
+ *
+ */
+public class RevertEnrichmentService implements IEnrichmentService {
+
+	public void appendFileToFile(File toFile, File fromFile) throws IOException {
+		StringBuilder fromContent = FileUtils.concatLines(fromFile);
+
+		fromContent.reverse();
+		
+		System.out.println(fromContent);
+		
+		FileUtils.appendToFile(toFile, fromContent.toString());
+	}
+
+}

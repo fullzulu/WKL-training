3506f339349b8176a3da56fedcc74e728dc85477
Aliaksandr_Pleski <fullzulu@gmail.com>
2014-01-27 18:26:03 +0300

    added log message

--- src/main/java/wkl/training/mvn/runner/Main.java
+++ src/main/java/wkl/training/mvn/runner/Main.java
@@ -15,8 +15,8 @@ import wkl.training.enrichment.file.service.impl.DirectEnrichmentService;
  * 
  */
 public class Main {
+	static Logger log = LogManager.getLogger(Main.class);
 	public static void main(String[] args) {
-		Logger log = LogManager.getLogger(Main.class);
 		if (args.length == 2) {
 			File toFile = getFile(args[0]);
 			File fromFile = getFile(args[1]);
@@ -36,6 +36,7 @@ public class Main {
 	
 	private static File getFile(String path){
 		File file = new File(path);
+		log.info(path);
 		if(!file.exists()){
 			file = new File(System.getProperty("user.dir") + File.separator + path);
 		}
